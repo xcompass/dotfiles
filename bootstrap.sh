edb23364d010d246253f74cdd9e3b695bb6dbb31
Pan Luo <pan.luo@ubc.ca>
2015-04-07 23:56:54 -0700

    Clean up and add my dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,22 +1,27 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")";
+# cd "$(dirname "${BASH_SOURCE}")";
 
-git pull origin master;
+# git pull origin master;
 
-function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
-	source ~/.bash_profile;
+DOTFILES_ROOT=$(pwd)
+
+install_dotfiles () {
+    for src in $(find $DOTFILES_ROOT -name '.*' ! -name '.git')
+    do
+        dst="$HOME/$(basename "${src}")"
+        echo "Linking " $src "->" $dst
+        ln -F -s "$src" "$dst"
+    done
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
+	install_dotfiles;
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
 	echo "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
+		install_dotfiles;
 	fi;
 fi;
 unset doIt;
