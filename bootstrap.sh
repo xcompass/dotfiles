f4c30b2206fff6c4c5d76baf1d9ec48bf4bac003
Pan Luo <pan.luo@ubc.ca>
2015-12-03 15:52:12 -0800

    Add uninstall option

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,8 +15,19 @@ install_dotfiles () {
     done
 }
 
+uninstall_dotfiles () {
+    for src in $(find $DOTFILES_ROOT -name '.*' ! -name '.git')
+    do
+        dst="$HOME/$(basename "${src}")"
+        echo "Unlinking" $dst
+        rm -f $dst
+    done
+}
+
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	install_dotfiles;
+elif [ "$1" == "--uninstall" -o "$1" == "-u" ]; then
+    uninstall_dotfiles;
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
 	echo "";
